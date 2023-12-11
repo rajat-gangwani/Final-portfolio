a2b2b4c1d03860943d41ca6a1455d2797ce20092
rajat <rajatgangwani99@gmail.com>
2023-12-11 20:59:21 +0530

    Add new certificate (Namaste React) and few css changes.

--- src/components/myLearnings/Mylearning.js
+++ src/components/myLearnings/Mylearning.js
@@ -1,50 +1,47 @@
-import React from 'react'
-import "./mylearningsstyle.css"
-import reactlco from "../../images/Certificates/react-lco.jpg"
-import htmlcsslco from "../../images/Certificates/html-css-lco.jpg"
-import backendlco from "../../images/Certificates/backend-lco.jpg"
-import { Carousel,Image } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./mylearningsstyle.css";
+import namastereact from "../../images/Certificates/namstereactcert.jpg";
+import reactlco from "../../images/Certificates/react-lco.jpg";
+import backendlco from "../../images/Certificates/backend-lco.jpg";
+import { Carousel, Image } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Mylearning() {
-    
-      return (
-        <div className='learingnew' >
-        <div className='learning' >
-        <Carousel  variant='dark' >
-  <Carousel.Item>
-    <Image
-    className='img-center'
-      width="50%"
-      height="50%"
-      src={htmlcsslco}
-      alt="First slide"
-    />
-   
-  </Carousel.Item>
-  <Carousel.Item>
-    <Image
-      className='img-center'
-    width="50%"
-    height="50%"
-      src={reactlco}
-      alt="Second slide"
-    />
-  </Carousel.Item>
-  <Carousel.Item>
-    <Image fluid
-      className='img-center'
-      width="50%"
-      height="50%"
-      src={backendlco}
-      alt="Third slide"
-    />
-
-  </Carousel.Item>
-</Carousel>
-</div>
-</div>
-      );
-    
+  return (
+    <div className="learingnew">
+      <div className="learning">
+        <Carousel variant="dark">
+          <Carousel.Item>
+            <Image
+              className="img-center"
+              width="50%"
+              height="50%"
+              src={namastereact}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              className="img-center"
+              width="50%"
+              height="50%"
+              src={reactlco}
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              fluid
+              className="img-center"
+              width="50%"
+              height="50%"
+              src={backendlco}
+              alt="Third slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
 }
 export default Mylearning;
